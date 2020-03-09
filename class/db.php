931d29d72b80d211f9a7c4689e8d45b3e0d6d30c
JgPhil <jamingph@gmail.com>
2020-03-09 16:11:39 +0100

    rename dbConnect

--- class/db.php
+++ class/db.php
@@ -2,14 +2,15 @@
 
 class database
 {
-public static function connect() {
+public static function dbConnect() {
 try{
-$pdo = new PDO('mysql:host=localhost;dbname=blog','root','', [
+$bd = new PDO('mysql:host=localhost;dbname=blog','root','', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 }
 catch(PDOException $e)
 {
 echo $e ;
 }
+return $db;
 }
 }
