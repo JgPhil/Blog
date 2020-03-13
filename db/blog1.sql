c6f8ba09dbae1834a91db0ed618fdb31dd6115d0
JgPhil <jamingph@gmail.com>
2020-03-13 10:46:45 +0100

    first sql request shown on post page, mvc working

--- db/blog1.sql
+++ db/blog1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 13 mars 2020 à 09:39
+-- Généré le :  ven. 13 mars 2020 à 09:45
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.4.0
 
