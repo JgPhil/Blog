262919a5ba8f8925cc87aa77dca3909e6eb498fe
JgPhil <jamingph@gmail.com>
2020-03-14 11:28:06 +0100

    0.9

--- App/Templates/post.php
+++ App/Templates/post.php
@@ -20,11 +20,12 @@ $comments = $c->getComments();
 
     <h1><?= $result['title'] ?></h1><br>
     <h3><?= $result['heading'] ?></h3><br>
-    <p><em><b><?= $author['username']?></b>, le <b><?= $result['creation_date']?>.</b></em><p><br>
-    <p><?= $result['content'] ?> </p><br>
-    <h5>Commentaires</h5>
+        <p><em><b><?= $author['username']?></b>, le <b><?= $result['creation_date']?>.</b></em><p><br>
+        <p><?= $result['content'] ?> </p><br>
+    
+    <h5>Commentaires</h5><br>
     <?php foreach ($comments as $comment) { ?>
-        <h6><?=$comment['user_id']?>, le <?=$comment['comment_date']?></h6><br>
+        <h6><em><b><?=$comment['user_id']?></b>, le <b><?=$comment['comment_date']?></em></b></h6>
         <p>
     <?= $comment['content_comment'];
     }
