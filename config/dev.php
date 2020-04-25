d782fdd49b6fa8a41b4090946ddb8bff894f5596
JgPhil <jamingph@gmail.com>
2020-04-25 10:28:33 +0200

    gitignore update

--- config/dev.php
+++ config/dev.php
@@ -1,24 +1,5 @@
 <?php
 
-//Connexion DATABASE
-const HOST = "localhost";
-const DB_NAME = 'blog';
-const CHARSET = 'utf8';
-const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
-const DB_USER = 'root';
-const DB_PASS = '';
-
-//PATH
-const MODEL_PATH = "App\\src\\model\\";
-const DAO_PATH = "App\\src\\DAO\\";
-const TEMPLATES_PATH = "../\\templates\\";
-const LAYOUT_PATH = "../\\templates\\layout.php";
-const INDEX_PATH = "../public/index.php";
-const SLUG = "?route=";
-const IMG_PATH = "../public/img/portfolio/";
-const CONSTRAINT_PATH = "App\\src\\constraint\\";
-
-const ADMIN_EMAIL_ADRESS = "blogjamingphilippe@gmail.com";
-const PASSWORD = "Flor@lysFr33";
+//
 
 
